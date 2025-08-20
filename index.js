c4729b1e3204f9cd92402151d7e2c845636d896e
ZiadAburas <ziad2018zzzz@gmail.com>
2025-08-20 20:51:24 +0300

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/form1', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'form1.html'));
 });
 
